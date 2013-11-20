a2df577b847e52ed2d7d3684c83136f40fe62423
Maciej Łebkowski <m.lebkowski@gmail.com>
2013-11-20 19:17:51 +0100

    setup redirects modifies `/etc/hosts`

--- src/Crane/Command/SetupRedirectionsCommand.php
+++ src/Crane/Command/SetupRedirectionsCommand.php
@@ -2,6 +2,7 @@
 
 namespace Crane\Command;
 
+use Cilex\Provider\Console\ContainerAwareApplication;
 use Crane\Docker\DuctTape;
 use Crane\Docker\Executor\ExecutorFactory;
 use Nassau\Silex\Command;
@@ -11,6 +12,9 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class SetupRedirectionsCommand extends Command
 {
+	private $version;
+	private $remoteIp;
+
 	protected function configure()
 	{
 		return $this->setName('setup:redirections')
@@ -25,8 +29,46 @@ class SetupRedirectionsCommand extends Command
 		$executor = $factory->createExecutor($output, true, $ssh);
 
 		$ductTape = new DuctTape;
-		var_dump($ductTape->getRemoteIp($executor));
+		$this->remoteIp = $ductTape->getRemoteIp($executor);
 
+		/** @var ContainerAwareApplication $console */
+		$console = $this->getApplication()->getService('console');
+		$this->version = strip_tags($console->getLongVersion());
+
+		$dpHosts = ['local.znanylekarz.pl', 'local.znamylekar.pl', 'local.docplanner.ru'];
+
+		$hosts = file('/etc/hosts');
+		$hosts = array_map(function ($line) use ($dpHosts)
+		{
+			if ("#" === substr(ltrim($line), 0, 1) || "" == ltrim($line))
+			{
+				return $line;
+			}
+			list ($ip, $host) = preg_split('/\s+/', trim($line));
+			if (false === in_array($host, $dpHosts))
+			{
+				return $line;
+			}
+
+			return "";
+		}, $hosts);
+
+		while (trim($hosts[sizeof($hosts)-1]) == "")
+		{
+			$hosts = array_slice($hosts, 0, -1);
+		}
+
+		$hosts[] = "\n";
+		foreach ($dpHosts as $host)
+		{
+			$hosts[] = $this->getLine($host);
+		}
+		file_put_contents("/etc/hosts", implode("", $hosts));
+	}
+
+	protected function getLine($host)
+	{
+		return sprintf("%s	%s	# %s\n", $this->remoteIp, $host, $this->version);
 	}
 
 }
